2ab8b790fc61edd13006fe448f3887282bf066c4
Percy <percy.cs.cheung@gmail.com>
2019-06-25 12:32:53 +0800

    change the layouts; add "render strokes"

--- lessons/index.php
+++ lessons/index.php
@@ -2,35 +2,40 @@
 <html lang="en-us">
 	<head>
 		<meta charset="utf-8">
-		<title>Hanzi Writer</title>
+		<title>学写字</title>
 		<!-- <link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'> -->
 		<link rel="stylesheet" href="styles.css" />
 		<script src="https://cdn.polyfill.io/v2/polyfill.min.js"></script>
-	</head>
+    </head>
 	<body>
 
-		<h1 class="title">Hanzi Writer</h1>
+		<h1 class="title">学写字</h1>
 		<form class="js-char-form char-form">
 			<label>
-				Character
-				<input type="text" class="js-char char-input" size="1" maxlength="1" value="我" />
+				<input type="text" class="js-char char-input" size="1" maxlength="1" value="仁" />
 			</label>
-			<button type="submit">Update</button>
+			<button type="submit">更新</button>
+			<button class="js-quiz">试写</button>
 		</form>
 		<div class="actions">
-			<button class="js-toggle">Show/hide</button>
-			<button class="js-toggle-hint">Outline on/off</button>
-			<button class="js-animate">Animate</button>
-			<button class="js-quiz">Quiz yourself</button>
+            <div class="js-toggle"></div>
+			<div class="js-toggle-hint"></div>
+            <div class="js-animate"></div>
 		</div>
 
 		<div id="target">
 
-		</div>
+        </div>
+
+        <br>
+
+        <div id="target-stroke">
+
+        </div>
 
 		<!-- <script src="https://cdn.polyfill.io/v2/polyfill.min.js"></script> -->
 		<script type="application/javascript" src="../dist/hanzi-writer.js"></script>
-		<script type="application/javascript" src="test.js"></script>
+		<script type="application/javascript" src="wrapper.js"></script>
 
 	</body>
 </html>
--- lessons/wrapper.js
+++ lessons/wrapper.js
@@ -22,8 +22,53 @@ function updateCharacter() {
 	});
 	isCharVisible = true;
 	isOutlineVisible = true;
-	window.writer = writer;
+    window.writer = writer;
+    RenderStoke(character);
 }
+
+
+function renderFanningStrokes(target, strokes) {
+    var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    width = 75;
+    height = 75;
+    svg.style.width = width + 'px';
+    svg.style.height = height + 'px';
+    svg.style.border = '1px solid #EEE'
+    svg.style.marginTop = '3px'
+    svg.style.marginLeft = '3px'
+    svg.style.marginRight = '3px'
+    target.appendChild(svg);
+    var group = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+
+    // set the transform property on the g element so the character renders at width x height (suggested 75x75)
+    var transformData = HanziWriter.getScalingTransform(width, height);
+    group.setAttributeNS(null, 'transform', transformData.transform);
+    svg.appendChild(group);
+
+    strokes.forEach(function (strokePath, idx, array) {
+        var path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+        path.setAttributeNS(null, 'd', strokePath);
+        // style the character paths
+        path.style.fill = '#555';
+        group.appendChild(path);
+    });
+}
+
+
+/**
+ * render the stroke according to example in https://chanind.github.io/hanzi-writer/docs.html
+ */
+function RenderStoke(character) {
+    document.querySelector('#target-stroke').innerHTML = '';
+    HanziWriter.loadCharacterData(character).then(function (charData) {
+        var target = document.getElementById('target-stroke');
+        for (var i = 0; i < charData.strokes.length; i++) {
+            var strokesPortion = charData.strokes.slice(0, i + 1);
+            renderFanningStrokes(target, strokesPortion);
+        }
+    });
+}
+
 
 window.onload = function() {
 	var char = decodeURIComponent(window.location.hash.slice(1));
@@ -35,7 +80,9 @@ window.onload = function() {
 
 	document.querySelector('.js-char-form').addEventListener('submit', function(evt) {
 		evt.preventDefault();
-		updateCharacter();
+        updateCharacter();
+        //auto write the character for every update
+        writer.animateCharacter();
 	});
 
 	document.querySelector('.js-toggle').addEventListener('click', function() {
@@ -46,12 +93,17 @@ window.onload = function() {
 		isOutlineVisible ? writer.hideOutline() : writer.showOutline();
 		isOutlineVisible = !isOutlineVisible;
 	});
-	document.querySelector('.js-animate').addEventListener('click', function() {
-		writer.animateCharacter();
+    document.querySelector('.js-animate').addEventListener('click', function () {
+        event.preventDefault()
+       writer.animateCharacter();
 	});
-	document.querySelector('.js-quiz').addEventListener('click', function() {
+    document.querySelector('.js-quiz').addEventListener('click', function () {
+        event.preventDefault()
 		writer.quiz({
 			showOutline: true
 		});
-	});
+    });
+
+    //when load the page, immediately write the character
+    writer.animateCharacter();
 }
