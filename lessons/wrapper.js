568b461f18826e403e1676cdb3fb1348983efd0b
Percy <percy.cs.cheung@gmail.com>
2019-06-25 13:36:53 +0800

    add "tian" grid for stroke box

--- lessons/wrapper.js
+++ lessons/wrapper.js
@@ -2,33 +2,34 @@ var writer;
 var isCharVisible;
 var isOutlineVisible;
 
+var grid_type = "Mi"; // [ "Mi", "Tian" ] ; might support ["9x9", "Hui"] if required;
+
 function printStrokePoints(data) {
-	var pointStrs = data.drawnPath.points.map(point => `{x: ${point.x}, y: ${point.y}}`);
-	console.log(`[${pointStrs.join(', ')}]`);
+    var pointStrs = data.drawnPath.points.map(point => `{x: ${point.x}, y: ${point.y}}`);
+    console.log(`[${pointStrs.join(', ')}]`);
 }
 
 function updateCharacter() {
-	document.querySelector('#target').innerHTML = '';
+    document.querySelector('#target').innerHTML = '';
 
-	var character = document.querySelector('.js-char').value;
-	window.location.hash = character;
-	writer = HanziWriter.create('target', character, {
-		width: 400,
-		height: 400,
-		renderer: 'canvas',
-		radicalColor: '#166E16',
-		onCorrectStroke: printStrokePoints,
-		onMistake: printStrokePoints,
-	});
-	isCharVisible = true;
-	isOutlineVisible = true;
+    var character = document.querySelector('.js-char').value;
+    window.location.hash = character;
+    writer = HanziWriter.create('target', character, {
+        width: 400,
+        height: 400,
+        renderer: 'canvas',
+        radicalColor: '#166E16',
+        onCorrectStroke: printStrokePoints,
+        onMistake: printStrokePoints,
+    });
+    isCharVisible = true;
+    isOutlineVisible = true;
     window.writer = writer;
     RenderStoke(character);
 }
 
 function addMiGrid(svg, width, height)
 {
-    //add grid method two:
     var newLine1 = document.createElementNS('http://www.w3.org/2000/svg', 'line');
     newLine1.setAttribute('id', 'line1');
     newLine1.setAttribute('x1', '0');
@@ -70,13 +71,40 @@ function addMiGrid(svg, width, height)
     svg.append(newLine4);
 }
 
-function addGrid(svg, type, width, height)
+function addTianGrid(svg, width, height) {
+    var newLine3 = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+    newLine3.setAttribute('id', 'line3');
+    newLine3.setAttribute('x1', width / 2);
+    newLine3.setAttribute('y1', '0');
+    newLine3.setAttribute('x2', width / 2);
+    newLine3.setAttribute('y2', height);
+    newLine3.setAttribute("stroke", "#DDD");
+    newLine3.setAttribute("stroke-dasharray", "3,3");
+    svg.append(newLine3);
+
+    var newLine4 = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+    newLine4.setAttribute('id', 'line4');
+    newLine4.setAttribute('x1', '0');
+    newLine4.setAttribute('y1', height / 2);
+    newLine4.setAttribute('x2', width);
+    newLine4.setAttribute('y2', height / 2);
+    newLine4.setAttribute("stroke-dasharray", "3,3");
+    newLine4.setAttribute("stroke", "#DDD")
+    svg.append(newLine4);
+}
+
+function addGrid(svg, width, height)
 {
-    switch (type)
+    switch (grid_type)
     {
         case "Mi":
+            addMiGrid(svg, width, height);
+            break;
+        case "Tian":
+            addTianGrid(svg, width, height);
+            break;
         default:
-        addMiGrid(svg, width, height);
+            //blank;
     }
 }
 
@@ -98,7 +126,7 @@ function renderFanningStrokes(target, strokes) {
     //svg.setAttribute('class', 'small');
 
     //add grid - method two: render via svg
-    addGrid(svg, 'Mi', width, height);
+    addGrid(svg, width, height);
 
     target.appendChild(svg);
     var group = document.createElementNS('http://www.w3.org/2000/svg', 'g');
@@ -136,39 +164,42 @@ function RenderStoke(character) {
 }
 
 
-window.onload = function() {
-	var char = decodeURIComponent(window.location.hash.slice(1));
-	if (char) {
-		document.querySelector('.js-char').value = char;
-	}
+window.onload = function () {
+    var char = decodeURIComponent(window.location.hash.slice(1));
+    if (char) {
+        document.querySelector('.js-char').value = char;
+    }
 
-	updateCharacter();
+    updateCharacter();
 
-	document.querySelector('.js-char-form').addEventListener('submit', function(evt) {
-		evt.preventDefault();
+    document.querySelector('.js-char-form').addEventListener('submit', function (evt) {
+        evt.preventDefault();
         updateCharacter();
         //auto write the character for every update
         writer.animateCharacter();
-	});
+    });
 
-	document.querySelector('.js-toggle').addEventListener('click', function() {
-		isCharVisible ? writer.hideCharacter() : writer.showCharacter();
-		isCharVisible = !isCharVisible;
-	});
-	document.querySelector('.js-toggle-hint').addEventListener('click', function() {
-		isOutlineVisible ? writer.hideOutline() : writer.showOutline();
-		isOutlineVisible = !isOutlineVisible;
-	});
+    document.querySelector('.js-toggle').addEventListener('click', function () {
+        isCharVisible ? writer.hideCharacter() : writer.showCharacter();
+        isCharVisible = !isCharVisible;
+    });
+    document.querySelector('.js-toggle-hint').addEventListener('click', function () {
+        isOutlineVisible ? writer.hideOutline() : writer.showOutline();
+        isOutlineVisible = !isOutlineVisible;
+    });
     document.querySelector('.js-animate').addEventListener('click', function () {
         event.preventDefault()
-       writer.animateCharacter();
-	});
+        writer.animateCharacter();
+    });
     document.querySelector('.js-quiz').addEventListener('click', function () {
         event.preventDefault()
-		writer.quiz({
-			showOutline: true
-		});
+        writer.quiz({
+            showOutline: true
+        });
     });
+
+    //add Handler to control the grid type;
+    grid_type = 'Mi';
 
     //when load the page, immediately write the character
     writer.animateCharacter();
